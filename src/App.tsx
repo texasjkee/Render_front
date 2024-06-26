a38b5a2065f641befeaec65773e76ab4c1088dcd
Michael <gt.gluck@gmail.com>
2024-06-26 10:41:17 +0300

    add: fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
+import { useEffect, useState } from 'react';
 import './App.css'
 
 function App() {
+  const [data, setData] = useState({ title: 'wow' });
+
+  useEffect(() => {
+    fetch('http://localhost:3000')
+      .then(response => response.json())
+      .then(json => setData(json))
+  }, [])
 
   return (
-   <h1>Yo render</h1> 
+    <>
+      <h1>Yo render</h1>
+      {data.title}
+      <button>
+        click
+      </button>
+    </>
   )
 }
 
