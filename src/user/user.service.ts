4ee74d93be4a7cbc5163e85014e84208fcfa4760
texasjkee <gt.gluck@gmail.com>
2024-07-15 15:03:52 +0300

    add: bcrypt

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,35 @@
 import { Injectable } from '@nestjs/common';
+import { type User } from '@prisma/client';
+import { genSaltSync, hashSync } from 'bcrypt';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private readonly prismaService: PrismaService) { }
+
+  save(user: Partial<User>) {
+    return this.prismaService.user.create({
+      data: {
+        email: user.email,
+        password: user.password,
+        roles: ['USER'],
+      },
+    });
+  }
+
+  findOne(idOrMail: string) {
+    return this.prismaService.user.findFirst({
+      where: {
+        OR: [{ id: idOrMail }, { email: idOrMail }],
+      },
+    });
+  }
+
+  delete(id: string) {
+    return this.prismaService.user.delete({ where: { id } });
+  }
+
+  private hashPassword(password: string) {
+    return hashSync(password, genSaltSync(10));
+  }
+}
